9ef9483d7d16967c43f73299923fe849f5a923d9
Vitaliy Slobodin <vitaliy.slobodin@gmail.com>
2013-04-29 23:56:05 +0400

    Enable platform auto-detection

--- preconfig.cmd
+++ preconfig.cmd
@@ -1,1 +1,1 @@
-configure -static -nomake demos -nomake tests -nomake examples -mp -no-cetest -no-iwmmxt -no-vcproj -no-angle -opengl desktop -nomake declarative -platform win32-msvc2012
+configure -static -nomake demos -nomake tests -nomake examples -mp -no-cetest -no-iwmmxt -no-vcproj -no-angle -opengl desktop -nomake declarative
