118c27462bc4bc7c166e99e2fdc84a2d5f28677d
Vitaliy Slobodin <vitaliy.slobodin@gmail.com>
2013-06-02 21:30:23 +0400

    Modified preconfig.cmd for MSVC2010

--- preconfig.cmd
+++ preconfig.cmd
@@ -1,1 +1,1 @@
-configure -static -nomake demos -nomake tests -nomake examples -mp -no-cetest -no-iwmmxt -no-vcproj -no-angle -opengl desktop -nomake declarative
+configure -static -nomake demos -nomake tests -nomake examples -mp -no-cetest -no-iwmmxt -no-vcproj -no-angle -opengl desktop -icu
