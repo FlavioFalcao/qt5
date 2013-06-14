4d8403f199098e8340af1b6a64534151f3a33121
Vitaliy Slobodin <vitaliy.slobodin@gmail.com>
2013-06-14 21:45:06 +0400

    Reformat preconfig.cmd

--- preconfig.cmd
+++ preconfig.cmd
@@ -1,1 +1,26 @@
-configure -static -opensource -nomake demos -nomake tests -nomake examples -mp -no-cetest -no-iwmmxt -no-vcproj -no-angle -opengl desktop -icu -qt-zlib -qt-libpng -qt-libjpeg -platform win32-msvc2010
+@echo off
+rem setup INCLUDE and LIB environment variables
+set %LIB%=%CD%\3rdparty\icu\lib
+set %INCLUDE%=%CD%\3rdparty\icu\include
+
+set QT_CONFIG=
+
+set QT_CONFIG=%QT_CONFIG% -debug
+set QT_CONFIG=%QT_CONFIG% -static
+set QT_CONFIG=%QT_CONFIG% -opensource
+set QT_CONFIG=%QT_CONFIG% -nomake demos
+set QT_CONFIG=%QT_CONFIG% -nomake tests
+set QT_CONFIG=%QT_CONFIG% -nomake examples
+set QT_CONFIG=%QT_CONFIG% -mp
+set QT_CONFIG=%QT_CONFIG% -no-cetest
+set QT_CONFIG=%QT_CONFIG% -no-iwmmxt
+set QT_CONFIG=%QT_CONFIG% -no-vcproj
+set QT_CONFIG=%QT_CONFIG% -no-angle
+set QT_CONFIG=%QT_CONFIG% -opengl desktop
+set QT_CONFIG=%QT_CONFIG% -icu
+set QT_CONFIG=%QT_CONFIG% -qt-zlib
+set QT_CONFIG=%QT_CONFIG% -qt-libpng
+set QT_CONFIG=%QT_CONFIG% -qt-libjpeg
+set QT_CONFIG=%QT_CONFIG% -platform win32-msvc2010
+
+configure %QT_CONFIG%
