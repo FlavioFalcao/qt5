406ea02827c3449e87dceee62779e5cc19c01723
Vitaliy Slobodin <vitaliy.slobodin@gmail.com>
2013-06-14 16:40:15 +0400

    Use Qt versions of Zlib, libpng and libjpeg

--- preconfig.cmd
+++ preconfig.cmd
@@ -1,1 +1,1 @@
-configure -static -nomake demos -nomake tests -nomake examples -mp -no-cetest -no-iwmmxt -no-vcproj -no-angle -opengl desktop -icu -platform win32-msvc2010
+configure -static -opensource -nomake demos -nomake tests -nomake examples -mp -no-cetest -no-iwmmxt -no-vcproj -no-angle -opengl desktop -icu -qt-zlib -qt-libpng -qt-libjpeg -platform win32-msvc2010
